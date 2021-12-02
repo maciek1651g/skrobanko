76971d96c4cce93eaee86b8ca38f8ef5a33718dc
Maciek <maciek1651g@gmail.com>
2021-12-02 15:20:17 +0100

    handle error

--- getPriceFromMoreleSite.js
+++ getPriceFromMoreleSite.js
@@ -1,13 +1,19 @@
 import fetch from "node-fetch";
 
 const getPriceFromMoreleSite = async (url) => {
-    const response = await fetch(url);
-    const body = await response.text();
+    const response = await fetch(url).catch((err) => console.log("Request error"));
+    if (response) {
+        const body = await response.text();
 
-    const reg = /id="product_price_brutto" itemprop="price" content="([\d\.]*)"/gm;
-    const arr = reg.exec(body);
-    const price = parseFloat(arr[1]);
-    return price;
+        const reg =
+            /id="product_price_brutto" itemprop="price" content="([\d\.]*)"/gm;
+        const arr = reg.exec(body);
+        if (arr) {
+            const price = parseFloat(arr[1]);
+            return price;
+        }
+    }
+    return null;
 };
 
 export default getPriceFromMoreleSite;
